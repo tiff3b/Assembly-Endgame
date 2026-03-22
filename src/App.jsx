a7ee957ff9c3b3690a264e5cc1cd7924b43c9de3
tiff3b <tiffany3berger@gmail.com>
2026-03-22 10:30:09 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Header from "../components/Header.jsx"
+
+export default function Endgame() {
+    return (
+        <>
+        <main>
+            <Header/>
+        </main>
+        </>
+    )
+}
