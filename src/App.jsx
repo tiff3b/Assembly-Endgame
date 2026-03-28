de47c4a0d3d9783d37049237a485120a03251bab
tiff3b <tiffany3berger@gmail.com>
2026-03-28 11:45:00 -0400

    Added game status and language elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
 import './App.css'
-import Header from "../components/Header.jsx"
+import languages from './language.js'
 
 export default function Endgame() {
+    
+    const languageElements = languages.map((lang) =>{
+        return(
+            <span 
+               key={lang.name}
+               name={lang.name}
+               style={{
+                backgroundColor: lang.backgroundColor,
+                color: lang.color
+            }}
+            >
+                {lang.name}
+            </span>   
+        )
+    })
+
     return (
-        <>
         <main>
-            <Header/>
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+            </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done!🎉</p>
+            </section>  
+            <section className="language-chips">
+                {languageElements}    
+            </section>     
         </main>
-        </>
     )
 }
